e0407773dcc80a01ccbb09b3716c340d3eaf4d6c
Ti1mmy <47431121+Ti1mmy@users.noreply.github.com>
2020-08-23 02:32:08 -0400

    output to txt file, any size team >1

--- Seeded Duo Generator.py
+++ Seeded Duo Generator.py
@@ -1,30 +1,48 @@
 import random
 print('Seeded Duo Generator')
-print('--------------------')
+print('------------------------------------------------------------------------')
+print('INFO: GENERATES SEEDED DUOS/GROUPS FROM LIST OF NAMES')
+print('note: will create .txt file inside parent directory of python file')
+print('------------------------------------------------------------------------')
 seed = input('Enter seed: ')
-print('--------------------')
-print('Paste list of names, separated by newlines (\n). You can do this easily in a word processor.')
-print('Once complete, press enter twice.')
+team_size = int(input('Team size: '))
+print()
+print('------------------------------------------------------------------------')
+print('Input player names. Once complete, leave next input empty (press enter).')
+print('------------------------------------------------------------------------')
+print()
 people = []
+number = 1
 while True:
-    individual = input()
-    if individual == "":
+    individual = input(f'Player {number}: ')
+    if not individual:
         break
     people.append(individual)
-teams = []
-i = 0
-random.seed(seed)
-while len(people):
-    team = random.sample(people, 2)
-    teams.append(team)
-    for person in team:
-        people.remove(person)
-    i += 1
-print(f'Teams are as follows for seed "{seed}":')
-print()
-k = 1
-for duo in teams:
-    print(f'Team {k}:')
-    print(f'{duo[0]} & {duo[1]}')
-    print()
-    k += 1
+    number += 1
+if len(people) < 2:
+    print('Error: More players needed')
+elif not len(people) // team_size:
+    print('Error: Not enough players to evenly distribute: try again.')
+else:
+    teams = []
+    i = 0
+    random.seed(seed)
+    while len(people):
+        team = random.sample(people, team_size)
+        teams.append(team)
+        for person in team:
+            people.remove(person)
+        i += 1
+    with open('Seeded Teams.txt', 'a', encoding='utf-8') as output:
+        output.write(f'Seeded Duo Generator\n')
+        output.write('------------------------------------------------------------------------\n')
+        output.write(f'Seed: {seed}\n')
+        output.write(f'Team Size: {team_size}\n')
+        output.write('------------------------------------------------------------------------\n')
+        output.write('\nTeams:\n')
+        k = 1
+        for group in teams:
+            output.write(f'\nTeam {k}:\n')
+            for member in group:
+                output.write(f'     - {member}\n')
+            k += 1
